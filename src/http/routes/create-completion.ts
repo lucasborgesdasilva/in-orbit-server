272811ff4cfbd077ac694afdd5adbb8570e94bdb
lucasborgesdasilva <lucasborges2812@gmail.com>
2024-10-03 13:28:27 -0300

    first commit

---
+++ src/http/routes/create-completion.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
+import { createGoalCompletion } from '../../use-cases/create-goal-completion';
+
+export const createCompletionRoute: FastifyPluginAsyncZod = async (app) => {
+  app.post(
+    '/completions',
+    {
+      schema: {
+        body: z.object({
+          goalId: z.string(),
+        }),
+      },
+    },
+    async req => {
+      const { goalId } = req.body
+      await createGoalCompletion({ goalId })
+    }
+  )
+};
---
+++ src/http/server.ts
@@ -0,0 +1,34 @@
+import fastify from "fastify";
+import {
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { createGoalRoute } from "./routes/create-goal";
+import { getPendingGoalsRoute } from "./routes/get-pending-goals";
+import { createCompletionRoute } from "./routes/create-completion";
+import { getWeekSummaryRoute } from "./routes/get-week.summary";
+import fastifyCors from "@fastify/cors";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+	origin: "*",
+});
+
+// Add schema validator and serializer
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createGoalRoute);
+app.register(getPendingGoalsRoute);
+app.register(createCompletionRoute);
+app.register(getWeekSummaryRoute);
+
+app
+	.listen({
+		port: 3333,
+	})
+	.then(() => {
+		console.log("HTTP Server running");
+	});
